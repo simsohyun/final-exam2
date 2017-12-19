eb68a8f171f5e4aa922feda89bec11fb3984d685
simsohyun <tlathgus15@gmail.com>
2017-12-19 11:23:59 +0900

    nemo.c

---
+++ coordinate_check.h
@@ -0,0 +1,12 @@
+int coordinate_check(int i, int j)
+{
+    extern char user[9][9];
+i+=3; j+=3;
+    if(user[i][j] == '*')
+        return 0;
+        else if(user[i][j] == 'O' || user[i][j] == 'X')
+        {
+            printf("  the coordinate already entered\n\n");
+            return 1;
+        }
+}
